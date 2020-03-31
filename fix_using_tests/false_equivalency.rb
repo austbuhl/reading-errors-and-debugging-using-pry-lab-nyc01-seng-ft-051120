e122b6df6f95c4e1e4e6947efd788c4da8087522
Austin Buhler <austinbuhler@gmail.com>
2020-03-31 19:14:03 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,14 +1,13 @@
 # don't forget to add: require 'pry'
-
-def get_user_input
-  gets.chomp
-end
+require 'pry'
 
 def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
+
+num = gets.chomp.to_i
 
 def selection(num)
   if num = 1
